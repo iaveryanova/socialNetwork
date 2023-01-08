32d06d5b6d3f0ba56953cd9e645aed1bb047ea1c
Iolanta Averyanova <iolantazhiznevskaya@gmail.com>
2023-01-09 01:25:09 +0300

    Code refactoring dialog-reducer, profile-reducer

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -4,45 +4,6 @@ import { updateNewPostTextActionCreator } from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
 import { connect } from 'react-redux';
 
-// let addPostActionCreator = () => {
-//   return {
-//     type: 'ADD-POST'
-//   }
-// }
-
-// let updateNewPostTextActionCreator = (text) => {
-//   return {
-//     type: 'UPDATE-NEW-POST-TEXT',
-//     newText: text
-//   }
-// }
-
-// const MyPostsContainer = (props) => {
-
-//   return (
-//     <StoreContext.Consumer>
-//       {(store) => {
-//         let state = store.getState();
-//         let addPost = () => {
-//           store.dispatch(addPostActionCreator());
-//         };
-      
-//         let onPostChange = (text) => {
-//           let action = updateNewPostTextActionCreator(text);
-//           store.dispatch(action);
-//         };
-//         return (
-//           <MyPosts
-//             updateNewPostsText={onPostChange}
-//             addPost={addPost}
-//             postsData={state.profilePage.postsData}
-//             newPostText={state.profilePage.newPostText}
-//           />
-//         )
-//       }}
-//     </StoreContext.Consumer>
-//   );
-// };
 
 let mapStateToProps = (state) => {
   return {  
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,59 +1,56 @@
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
-const SEND_MESSAGE = 'SEND-MESSAGE';
+const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
+const SEND_MESSAGE = "SEND-MESSAGE";
 
 let initialState = {
-    dialogsData: [
-      { id: 1, name: "Dimych" },
-      { id: 2, name: "Andrey" },
-      { id: 3, name: "Sveta" },
-      { id: 4, name: "Sasha" },
-      { id: 5, name: "Victor" },
-      { id: 6, name: "Valera" },
-    ],
-    messagesData: [
-      { id: 1, message: "Hi!" },
-      { id: 2, message: "How are you?" },
-      { id: 3, message: "I love you" },
-      { id: 4, message: "Yo" },
-    ],
-    newMessageBody: '',
-}
+  dialogsData: [
+    { id: 1, name: "Dimych" },
+    { id: 2, name: "Andrey" },
+    { id: 3, name: "Sveta" },
+    { id: 4, name: "Sasha" },
+    { id: 5, name: "Victor" },
+    { id: 6, name: "Valera" },
+  ],
+  messagesData: [
+    { id: 1, message: "Hi!" },
+    { id: 2, message: "How are you?" },
+    { id: 3, message: "I love you" },
+    { id: 4, message: "Yo" },
+  ],
+  newMessageBody: "",
+};
 
 const dialogsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case UPDATE_NEW_MESSAGE_BODY:
+      return {
+        ...state,
+        newMessageBody: action.body,
+      };
 
-  let stateCopy = {...state};
-  // stateCopy.messagesData = [...state.messagesData]
+    case SEND_MESSAGE:
+      let body = state.newMessageBody;
+      return {
+        ...state,
+        newMessageBody: '',
+        messagesData: [...state.messagesData, { id: 5, message: body }],
+      };
 
-    switch (action.type) {
-        case UPDATE_NEW_MESSAGE_BODY: {
-          stateCopy.newMessageBody = action.body;
-          return stateCopy;
-        }
-  
-    
-        case SEND_MESSAGE: {
-            let body = state.newMessageBody;
-            stateCopy.newMessageBody = '';
-            stateCopy.messagesData.push( {id: 5, message: body} );
-            return stateCopy;
-        }
-            
-        default:
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};
 
 export const sendMessageActionCreator = () => {
-    return {
-      type: SEND_MESSAGE
-    }
-  }
-  
+  return {
+    type: SEND_MESSAGE,
+  };
+};
+
 export const updateNewMessageBodyActionCreator = (body) => {
-return {
-    type: UPDATE_NEW_MESSAGE_BODY, 
-    body: body
-}
-}
+  return {
+    type: UPDATE_NEW_MESSAGE_BODY,
+    body: body,
+  };
+};
 
 export default dialogsReducer;
